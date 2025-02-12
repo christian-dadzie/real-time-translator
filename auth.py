6b98fec3f09786e2daf9fe49afb353caedaed86d
christian-dadzie <161411979+christian-dadzie@users.noreply.github.com>
2025-02-12 03:05:52 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User  # Import the db instance and User model
+from models import db, User  
 from forms import RegistrationForm, LoginForm
 
 auth = Blueprint('auth', __name__)
@@ -12,9 +12,9 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and check_password_hash(user.password, form.password.data):
-            login_user(user)  # Log in the user
+            login_user(user)  
             flash('Login successful!', 'success')
-            return redirect(url_for('index'))  # Redirect to the index page
+            return redirect(url_for('index')) 
         else:
             flash('Login failed. Check your username and/or password.', 'danger')
     return render_template('login.html', form=form)
