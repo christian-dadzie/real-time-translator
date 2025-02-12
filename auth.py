e5b3c0283a08d6156447fd46b234d9a33cb4b217
christian-dadzie <161411979+christian-dadzie@users.noreply.github.com>
2025-02-12 02:48:28 +0000

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User  # Import the db instance and User model
+from forms import RegistrationForm, LoginForm
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)  # Log in the user
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))  # Redirect to the index page
+        else:
+            flash('Login failed. Check your username and/or password.', 'danger')
+    return render_template('login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('auth.login'))
